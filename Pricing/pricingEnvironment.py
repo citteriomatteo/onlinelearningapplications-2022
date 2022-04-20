f9d233f5bd1490c82cf7060957df382c77c2dab3
Carlo <carlo.cominoli@mail.polimi.it>
2022-04-20 16:51:00 +0200

    environment pricing part

---
+++ Pricing/pricingEnvironment.py
@@ -0,0 +1,18 @@
+import numpy as np
+from user_data_generator import StandardDataGenerator
+
+
+class Environment:
+    def __init__(self, n_arms, probabilities, resources="Pricing/user_data_generator.py"):
+        self.n_arms = n_arms
+        self.probabilities = probabilities
+
+        self.user_data = StandardDataGenerator(resources)
+        self.alpha_ratios = self.user_data.get_alpha_ratios()
+        self.num_daily_users = self.user_data.get_num_daily_users()
+        self.num_product_sold = self.user_data.get_num_product_sold()
+        self.features = self.user_data.get_features()
+        self.classes = self.user_data.get_classes()
+
+    def round(self, pulled_arm):
+        return np.random.binomial(1, self.probabilities[pulled_arm])
---
+++ Pricing/user_data_generator.py
@@ -0,0 +1,50 @@
+import json
+
+
+class StandardDataGenerator:
+
+    def __init__(self, filename):
+
+        self._filename = filename
+        self._data = None
+        with open(self._filename) as f:
+            self._data = json.load(f)
+        # features
+        self._features = self._data['features']
+        # classes
+        self._classes = {}
+        # class distributions
+        # self._class_distribution = []
+        for key in self._data['classes']:
+            self._classes[key] = {}
+            self._classes[key]['features'] = self._data['classes'][key]['features']
+            self._classes[key]['fraction'] = self._data['classes'][key]['fraction']
+
+        # alpha ratios
+        self._alpha_ratios = self._data['alpha_ratios']
+        # number of daily users
+        self._num_daily_users = self._data['num_daily_users']
+        # number of product sold
+        self._num_product_sold = self._data['num_product_sold']
+
+    def get_source(self) -> str:
+        return self._filename
+
+    def get_all(self):
+        return self._data
+
+    def get_features(self):
+        return self._features
+
+    def get_classes(self):
+        return self._classes
+
+    def get_alpha_ratios(self):
+        return self._alpha_ratios
+
+    def get_num_daily_users(self):
+        return self._num_daily_users
+
+    def get_num_product_sold(self):
+        return self._num_product_sold
+
