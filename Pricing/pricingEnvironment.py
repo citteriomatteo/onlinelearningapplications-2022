f7680a580f17a0ed7f4b37b288b16c37cd73618d
LorenzoCutrupi <lorenzo.cutrupi@mail.polimi.it>
2022-04-26 22:12:27 +0200

    Added graph to pricing environment

--- Pricing/pricingEnvironment.py
+++ Pricing/pricingEnvironment.py
@@ -1,9 +1,11 @@
 import numpy as np
+
+import Settings
 from user_data_generator import StandardDataGenerator
 
 
 class Environment:
-    def __init__(self, n_arms, probabilities, resources="../json/dataUsers.json"):
+    def __init__(self, n_arms, graph, probabilities, resources="../json/dataUsers.json"):
         self.n_arms = n_arms
         self.probabilities = probabilities
 
@@ -13,6 +15,14 @@ class Environment:
         self.num_product_sold = self.user_data.get_num_product_sold()
         self.features = self.user_data.get_features()
         self.classes = self.user_data.get_classes()
+
+        #TODO: discuss about this part
+        self.graph = graph
+        self.theta = np.random.dirichlet(np.ones(len(self.graph.nodes)), size=1)
+        self.arms_features = np.random.binomial(1, 0.5, size=(len(self.graph.edges), len(self.graph.nodes)))
+        self.lam = Settings.lam
+        for i in range(0, len(self.graph.edges)):
+            self.graph.edges[i].probability = np.dot(self.theta, self.arms_features[i])
 
     def round(self, pulled_arm):
         return np.random.binomial(1, self.probabilities[pulled_arm])
