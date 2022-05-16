b22fe3bf5d9c407b0c335ecbaf337152417603b0
Carlo <carlo.cominoli@mail.polimi.it>
2022-05-16 15:41:09 +0200

    TS algo implemented

--- Pricing/TS.py
+++ Pricing/TS.py
@@ -1,5 +1,7 @@
 import numpy as np
 from Learner import *
+from Pricing.pricingEnvironment import PricingEnvironment
+from Social_Influence.Graph import Graph
 
 
 class TS(Learner):
@@ -7,17 +9,29 @@ class TS(Learner):
     def __init__(self, n_arms, prices):
         super().__init__(n_arms, len(prices))
         self.prices = prices
-        self.means = np.zeros(self.prices.shape)
-        self.widths = np.ones(self.prices.shape) * np.inf
-        self.beta_parameters = np.ones(n_arms, 2)
-        self.t = 0
+        self.beta_parameters = np.ones((self.n_products, n_arms, 2))
 
     def pull_arm(self):
-        idx = np.argmax(np.random.beta(self.beta_parameters[:, 0], self.beta_parameters[:, 1]))
+        #idx = np.argmax(np.random.beta(self.beta_parameters[:, 0], self.beta_parameters[:, 1]))
+
+        idx = [0 for i in range(self.n_products)]
+        for prod in range(self.n_products):
+            idx[prod] = np.argmax(np.random.beta(self.beta_parameters[prod,:, 0], self.beta_parameters[prod,:, 1]))
         return idx
 
     def update(self, pulled_arm, reward):
-        self.t += 1
         super(TS, self).update(pulled_arm, reward)
-        self.beta_parameters[pulled_arm, 0] = self.beta_parameters[pulled_arm, 0] + reward
-        self.beta_parameters[pulled_arm, 1] = self.beta_parameters[pulled_arm, 1] + 1.0 - reward
+        for prod in range(self.n_products):
+            self.beta_parameters[prod, pulled_arm[prod], 0] = self.beta_parameters[prod, pulled_arm[prod], 0] + reward[prod]
+            self.beta_parameters[prod, pulled_arm[prod], 1] = self.beta_parameters[prod, pulled_arm[prod], 1] + 1.0 - reward[prod]
+
+
+graph = Graph(mode="full", weights=True)
+env = PricingEnvironment(4, graph, 1)
+learner = TS(4, env.prices[0])
+
+for i in range(10000):
+    pulled_arms = learner.pull_arm()
+    rewards = env.round(pulled_arms)
+    learner.update(pulled_arms, rewards)
+    print(pulled_arms)
