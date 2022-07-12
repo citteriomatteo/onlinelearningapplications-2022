462caadeeeb55e3f0f6da8217773b19612a1d1ba
Carlo <carlo.cominoli@mail.polimi.it>
2022-07-12 21:34:34 +0200

    SW UCB fixed

--- Settings.py
+++ Settings.py
@@ -6,6 +6,9 @@ LAMBDA = 0.7
 # Number of days to test
 NUM_OF_DAYS = 1000
 
+# Window size of days for Sliding Window (proportional to the square root of NUM_OF_DAYS)
+WINDOW_SIZE = int(np.sqrt(NUM_OF_DAYS))
+
 # Number of user interactions per day
 DAILY_INTERACTIONS = 500
 
